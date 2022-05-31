d3176fd8acec3e3ceed95b2a1fe6ad1fe25a22c4
edusamico <edusamico@gmail.com>
2022-05-31 02:19:27 -0300

    using response status to alert error

--- GitHub-user-search-app/my-app/src/services/api.jsx
+++ GitHub-user-search-app/my-app/src/services/api.jsx
@@ -3,12 +3,29 @@ const urlBase = "https://api.github.com/users/";
 export const api = {
     getDataUser: async (textInput) => {
         if (textInput === "") {
-            let body = {message : "Type some username"};
+            let body = { message: "Type some username" };
             return (body);
         }
         let response = await fetch(urlBase + textInput);
         let body = await response.json();
-        
+
+        if (response.status !== 200) {
+            if (response.status === 304) {
+                body = { message: `${response.status} Error: Not modified` };
+            }
+            if (response.status === 401) {
+                body = { message: `${response.status} Error: Requires authentication` };
+            }
+            if (response.status === 403) {
+                body = { message: `${response.status} Error: Forbidden` };
+            }
+            if (response.status === 404) {
+                body = { message: `${response.status} Error: Resource not found` };
+            }
+            if (response.status === 422) {
+                body = { message: `${response.status} Error: Validation failed` };
+            }
+        } 
         return body;
     }
 }
